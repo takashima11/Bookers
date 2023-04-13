d617f2d2fbd5cf34abfeadb00d9b7ff7a0bafddf
takashima11 <129072192+takashima11@users.noreply.github.com>
2023-04-13 14:31:37 +0000

    [Add]3-13 indexページのレイアウトなど

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,17 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/show'
   end
 
   def show
@@ -10,4 +19,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/new'
 
-
-
-  get '/' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show',as: 'books'
-  get 'books/:id/edit' => 'books#edit',as: 'edit_book'
-  patch 'books/:id' => 'books#update',as: 'update_book'
-  delete 'books/:id' => 'books#destroy',as: 'destroy_book'
+root to: 'homes#top'
+resources :books
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
