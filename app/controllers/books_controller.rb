e63209e52b00a3807807aad587feb086d649be81
takashima11 <129072192+takashima11@users.noreply.github.com>
2023-04-15 09:44:07 +0000

    [Add]3-13 editページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    @book = Book.new
+
   end
 
   def create
@@ -15,15 +15,23 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title,:body)
   end
-  
+
 end
