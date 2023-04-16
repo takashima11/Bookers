6829e33d10c46004416728df189f300b56aeb1bd
takashima11 <129072192+takashima11@users.noreply.github.com>
2023-04-16 14:19:50 +0000

    [Add]3-13 フラッシュメッセージまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,13 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+     flash[:notice] = "Book was successfully created."
+     redirect_to book_path(book.id)
+    else
+      @books = Book.all
+      render:index
+    end
   end
 
   def show
@@ -24,13 +29,19 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+     flash[:notice] = "Book was successfully updated."
+     redirect_to book_path(book.id)
+    else
+     @book = Book.find(params[:id])
+     render:edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
