e0b556f5b87ba495527238599791bedd62787603
takashima11 <129072192+takashima11@users.noreply.github.com>
2023-04-17 14:04:09 +0000

    [Finish]3-13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,13 +9,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
      flash[:notice] = "Book was successfully created."
-     redirect_to book_path(book.id)
+     redirect_to book_path(@book.id)
     else
       @books = Book.all
-      render:index
+
+      render :index
     end
   end
 
@@ -28,12 +29,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
      flash[:notice] = "Book was successfully updated."
-     redirect_to book_path(book.id)
+     redirect_to book_path(@book.id)
     else
-     @book = Book.find(params[:id])
+
      render:edit
     end
   end
